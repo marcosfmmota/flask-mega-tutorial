1228db3b784eb569618f33a99336887ab4ae9b9c
marcosfmmota <marcosfmmota@gmail.com>
2019-02-17 22:46:30 -0300

    Chapter 2 done

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,21 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username':'marcos'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username':'John'},
+            'body' : 'Beautiful day in Poland'
+        },
+        {
+            'author' : {'username' : 'Susan'},
+            'body' : 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username' : 'Marcos'},
+            'body' : 'Nobody cares what you think.'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
 @app.route('/info')
 def info():
